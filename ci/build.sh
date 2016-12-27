b1feba1bcaf461e408cf5bd2b477d8569cdd0b4b
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2016-12-27 12:55:54 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -3,10 +3,12 @@
 set -e -x
 
 git clone sample-app resource-app
-
+echo "creating file"
 cd resource-app
-
+echo "created the file"
 mvn clean compile
-
+echo " done compiling"
 mvn install
+echo "done installing"
 mvn test
+echo "done testing"
