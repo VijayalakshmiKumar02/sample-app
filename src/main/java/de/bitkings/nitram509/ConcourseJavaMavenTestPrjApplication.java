9c4ef3148fdb8106f164a4dac4033acc9a8c17a2
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2016-12-27 17:07:32 +0530

    Update ConcourseJavaMavenTestPrjApplication.java

--- src/main/java/de/bitkings/nitram509/ConcourseJavaMavenTestPrjApplication.java
+++ src/main/java/de/bitkings/nitram509/ConcourseJavaMavenTestPrjApplication.java
@@ -1,1 +1,13 @@
 
+package de.bitkings.nitram509;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcourseJavaMavenTestPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcourseJavaMavenTestPrjApplication.class, args);
+    }
+}
