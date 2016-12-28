abdfd1e3b41f569a87e023af83f962dcef58e941
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2016-12-28 11:40:51 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -6,10 +6,10 @@ git clone sample-app resource-app
 echo "creating file"
 cd resource-app
 echo "created the file"
-mvn clean compile
-echo " done compiling"
+#mvn clean compile
+#echo " done compiling"
 mvn install
 echo "done installing"
 #java de/bitkings/nitram509/ConcourseJavaMavenTestPrjApplication
-mvn package 
-echo "done running"
+mvn test
+echo "done testing"
