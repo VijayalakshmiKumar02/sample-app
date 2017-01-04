716fd6a516287bc537dba07339e6c8fac244048b
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-01-04 10:50:15 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -6,7 +6,7 @@ git clone sample-app resource-app
 echo "creating file"
 cd resource-app
 echo "created the file"
-#mvn clean compile
+mvn clean compile
 #echo " done compiling"
 #mvn install
 echo "done installing"
