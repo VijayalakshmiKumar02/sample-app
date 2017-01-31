f0c759308a521ab06f15bab7af68d7ab01fd09bd
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-01-31 15:34:23 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -5,15 +5,16 @@ set -e -x
 git clone sample-app resource-app
 echo "creating file"
 cd resource-app
+mvn clean package
 #echo "created the file"
 #mvn clean compile
 #mvn package
 #echo " done compiling"
-mvn install
-echo "done installing"
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Pcoverage-per-test
-mvn sonar:sonar
+#mvn install
+#echo "done installing"
+#mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install
+#mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Pcoverage-per-test
+#mvn sonar:sonar
 #java de/bitkings/nitram509/ConcourseJavaMavenTestPrjApplication
 #mvn test
 echo "done"
